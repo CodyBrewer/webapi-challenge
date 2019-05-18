3e9a360f0c4a4a4fb81ca1e35964f2d470e94216
Cody Brewer <brewer.cody.m@gmail.com>
2019-05-18 13:38:24 -0600

    delete works

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -40,4 +40,21 @@ actionRouter.post("/", validateProject, requiredBody, async (req, res) => {
     });
   }
 });
+actionRouter.delete("/:id", validateActionId, async (req, res) => {
+  try {
+    const count = await Action.remove(req.params.id);
+    if (count > 0) {
+      res.status(200).json({ message: "The action has been cancelled" });
+    } else {
+      res.status(404).json({ message: "The action could not be found" });
+    }
+  } catch (error) {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: "Error cancelling the action"
+    });
+  }
+});
+
 module.exports = actionRouter;
