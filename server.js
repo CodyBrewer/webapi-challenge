cb5077164f4c0f045b10e1a8061351e08eb61be3
Cody Brewer <brewer.cody.m@gmail.com>
2019-05-19 12:09:31 -0600

    got rid of conflicting loc files

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const helmet = require("helmet");
 const logger = require("morgan");
+const cors = require("cors");
 
 const projectRouter = require("./data/helpers/projectRouter.js");
 const actionRouter = require("./data/helpers/actionRouter.js");
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 server.use(helmet());
 server.use(logger("dev"));
 
@@ -17,6 +19,7 @@ server.get("/api", (req, res) => {
   res.send(`
     <h2> Welcome to API</h2>
     `);
+  res;
 });
 
 module.exports = server;
